67fbafd7e09256bc22c6d0d3204b7f5e0f8fdd8f
Chuckeyzz <69422746+Chuckeyzz@users.noreply.github.com>
2025-03-14 15:11:26 +0100

    complete version, passing all tests

--- lab3/node.c
+++ lab3/node.c
@@ -7,36 +7,34 @@ void rtinit(struct distance_table *table, int node) {
 }
 
 void rtupdate(struct distance_table *table, int node, struct rtpkt *pkt) {
-	bool updated = false;
+	bool changed = false;
 	int source = node;
+	int via_node = pkt->sourceid;
 	struct distance_table *updates = malloc(sizeof(struct distance_table));
 
 	//initialize the distance table "updates" to make sure it's not filled with garbage
 	memcpy(updates, table, sizeof(struct distance_table));
-
-	//rn only updates if new cost is better
-	for (int i = 0; i < 4; i++){
-		if(is_neighbor(source, i)){
-			for (int j = 0; j < 4; j++){
-				int new_cost = table->costs[source][i] + pkt->mincost[j];
-				if(updates->costs[source][j] > new_cost){
-					updates->costs[source][j] = new_cost;
-				}
+	//purpose of updates table is to prevent corruptions of data and garbage values in *table  
+	
+	//update the cost if a better one exists
+	for (int dest = 0; dest < 4; dest++){
+		if(updates->costs[via_node][dest] > pkt->mincost[dest]){
+				updates->costs[via_node][dest] = pkt->mincost[dest];
 			}
 		}
-	}
 	//actually update distance table
-	for (int i = 0; i < 4; i++){
-			for (int j = 0; j < 4; j++){
-				if(updates->costs[i][j] != table->costs[i][j]){
-					updated = true;					
-					table->costs[i][j] = updates->costs[i][j];
+	for (int via = 0; via < 4; via++){
+			for (int dest = 0; dest < 4; dest++){
+				if(updates->costs[via][dest] != table->costs[via][dest]){
+					changed = true;					
+					table->costs[via][dest] = updates->costs[via][dest];
 				}
 			}
 		}
-	if(updated){
-		sendpkt(updates, node);
-		updated = false;
+	//if the distance table has been changed, sent it to neighbours
+	if(changed){
+		sendpkt(table, node);
+		changed = false;
 	}
 	free(updates);
 	printdt(table, node);
@@ -44,22 +42,28 @@ void rtupdate(struct distance_table *table, int node, struct rtpkt *pkt) {
 
 void sendpkt(struct distance_table *table,int node){
 	int source = node;
-	struct rtpkt distpkt;			//create pkt
+	struct rtpkt distpkt;								//create pkt
 	distpkt.sourceid = source;							//set sourceID
-	//construct mincost & destinationID for pkt to be sent to neighbours
 	
-	for(int i = 0; i < 4; i++){
-		if(is_neighbor(source, i)){
-			printf("Setting mincost[%d] = table->costs[%d][%d] = %d\n", i, source, i, table->costs[source][i]);
-			distpkt.mincost[i] = table->costs[source][i];	
-			
+	//construct mincost & destID for pkt to be sent to neighbours (changed because of lab specification checks, thanks chatGPT)
+	for (int d = 0; d < 4; d++){
+		int best = INF;
+		for (int via = 0; via < 4; via++){
+			int cost;
+			if(via == node){
+				cost = table->costs[node][d];
+			}
+			else {
+				cost = table->costs[node][via] + table->costs[via][d];
+			}
+			if(cost < best){
+				best = cost;
+			}
 		}
-		else if(!is_neighbor(source, i)){
-			if (source == i) {distpkt.mincost[i] = 0;}
-			else {distpkt.mincost[i] = INF;}
-		}
+		distpkt.mincost[d] = best;
 	}
-	for (int i = 0; i < 4; i++){
+	//send the packets "UP UP AND AWAY"
+	for (int i = 0; i < 4; i++){ 
 		if (is_neighbor(source, i)){
 			distpkt.destid = i;	
 			tolayer2(distpkt);
